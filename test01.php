ea7492d1b4ca17de982ea1698c57499ae17ff2a4
gust <152129559+Thegustzazaa@users.noreply.github.com>
2024-02-07 11:02:43 +0700

    Add files via upload

--- test01.php
+++ test01.php
@@ -1,22 +1,25 @@
+<!DOCTYPE html>
 <html>
 <head>
-    <title>แสดงการสร้างและใช้งานอารเรย์ Numberic Array</title>
+    <title>แสดงการสร้างและใช้งานอารเรย์ Numeric Array</title>
 </head>
 <body>
     <?php
-        $MaxStudent = 10;
+        $MaxStudent = 20;
         for($n = 0 ; $n < $MaxStudent; $n++) {
             $score[$n] = rand(0, 100);
         }
         echo '<center><font size="4" color="blue"> Grade Report </font></center>';
         echo '<table border="1" align="center">';
         echo '<tr><th align="center">Student No.</th>';
-        echo '<th align="center">Score</th></tr>';
+        echo '<th align="center">Score</th>';
+        echo '<th align="center">Grade</th></tr>';
         for($n = 0 ; $n < $MaxStudent; $n++) {
             echo '<tr><td align="center">' . ($n +1 ) . '</td>';
-            echo '<td align="center">' . $score[$n] . '</td></tr>';
+            echo '<td align="center">' . $score[$n] . '</td>';
+            echo '<td align="center">' . calculateGrade($score[$n]) . '</td></tr>';
         }
-        echo '<tr><td colspan="2" align="center"> Average Score : ';
+        echo '<tr><td colspan="3" align="center"> Average Score : ';
         echo average( $score, $MaxStudent);
         echo '</td></tr></table>';
         
@@ -25,7 +28,30 @@
             for($n = 0 ; $n < $max; $n++) {
                 $total += $data[$n];
             }
-            return( $total / $max );
+            $average = $total / $max;
+            return round($average, 2);
+        }
+
+        function calculateGrade($score) {
+            if ($score >= 85) {
+                return 'A+';
+            } elseif ($score >= 80) {
+                return 'A';
+            } elseif ($score >= 75) {
+                return 'B+';
+            } elseif ($score >= 70) {
+                return 'B';
+            } elseif ($score >= 65) {
+                return 'C+';
+            } elseif ($score >= 60) {
+                return 'C';
+            } elseif ($score >= 55) {
+                return 'D+';
+            } elseif ($score >= 50) {
+                return 'D';
+            } else {
+                return 'F';
+            }
         }
     ?>
 </body>
